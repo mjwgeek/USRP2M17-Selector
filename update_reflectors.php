891e1e4b0606b526d9c309d160604f795332806a
Mike Webb <83971391+mjwgeek@users.noreply.github.com>
2024-09-27 16:06:44 -0500

    Fixed Update PHP Script

--- update_reflectors.php
+++ update_reflectors.php
@@ -1,7 +1,7 @@
 <?php
 header('Content-Type: application/json');
 
-$output = shell_exec('python3 /var/www/html/m17/update/update.py 2>&1');
+$output = shell_exec('python3 update.py 2>&1');
 if ($output === null) {
     echo json_encode(['status' => 'error', 'message' => 'Failed to execute script.']);
 } else {
